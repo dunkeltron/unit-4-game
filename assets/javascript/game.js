ff082464da5002d9af3424bb1079a3544060866d
Ian Dunkelberg <idunkelberg@gmail.com>
2018-09-05 01:57:42 -0700

    fixed logic error after defeat allowing multiple click events to register

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,44 +8,24 @@ var enemyChar;
 var numCharRemaining;
 var phase = 0;
 
+
 $(document).ready(function () {
+    startGame();
 
-
+});
+function startGame(){
     obiWan = { name: "Obi-Wan Kenobi", hP: 120, iAp: 8, aP: 8, cAP: 12, inPlay: false, image: "obi-wan" };
     darthMaul = { name: "Darth Maul", hP: 180, iAp: 4, aP: 4, cAP: 25, inPlay: false, image: "maul" };
     darthSidious = { name: "Darth Sidious", hP: 150, iAp: 6, aP: 6, cAP: 20, inPlay: false, image: "sidious" };
     lukeSkywalker = { name: "Luke Skywalker", hP: 100, iAp: 100, aP: 100, cAP: 5, inPlay: false, image: "luke" };
     characterArray = [obiWan, darthMaul, darthSidious, lukeSkywalker];
     numCharRemaining = characterArray.length;
-    imgSrc = '<img src=assets/img/' + lukeSkywalker.image + '.jpg>';
-  
-    function availableChoices() {
-
-        characterArray.forEach(function (element) {
-            var newBox = $("<div>");
-            var nameBox = $("<div>");
-            var picBox=$("<div>");
-            var hpBox= $("<div>");
-            nameBox.text(element.name);
-            nameBox.addClass("name-box");
-            
-            picBox.html("<img src=../img/"+element.image+".jpg");
-            picBox.addClass("picture-box");
-
-            hpBox.text("HP = "+element.hP);
-            hpBox.addClass("hp-box");
-            newBox.addClass(element.image);
-            //newBox.innerHtml("<img src=../img/"+element.image+".jpg");
-            newBox.attr('id', "selection-Zone");
-            newBox.append(nameBox);            
-            newBox.append(picBox);
-            newBox.append(hpBox);
-            $("#placeholder-Zone").append(newBox);
-        });
-    }
+    imgSrc = '<img src=assets/img/' + lukeSkywalker.image + '.jpg>';  
+    
 
     availableChoices();
     $(".luke").click(function () {
+        console.log("luke");
         if (phase === 0) {
             chooseChar(lukeSkywalker);
         }
@@ -78,23 +58,43 @@ $(document).ready(function () {
         }
 
     });
-    $("#fight-Button").click(function () {
-        console.log(phase);
-        if (phase === 2) {
-            fight();
-        }
-    })
-});
+        $("#fight-Button").click(function () {
+            console.log(phase);
+            if (phase === 2) {
+                fight();
+            }
+        })
+
+}
+function availableChoices() {
+
+    characterArray.forEach(function (element) {
+        var newBox = $("<div>");
+        var nameBox = $("<div>");
+        var picBox=$("<div>");
+        var hpBox= $("<div>");
+        nameBox.text(element.name);
+        nameBox.addClass("name-box");
+        
+        picBox.html("<img src=../img/"+element.image+".jpg");
+        picBox.addClass("picture-box");
+
+        hpBox.text("HP = "+element.hP);
+        hpBox.addClass("hp-box");
+        newBox.addClass(element.image);
+        //newBox.innerHtml("<img src=../img/"+element.image+".jpg");
+        newBox.attr('id', "selection-Zone");
+        newBox.append(nameBox);            
+        newBox.append(picBox);
+        newBox.append(hpBox);
+        $("#placeholder-Zone").append(newBox);
+    });
+}
 function reset() {
-    obiWan = { name: "Obi-Wan Kenobi", hP: 120, iAp: 8, aP: 8, cAP: 12, image: "obi-wan" };
-    darthMaul = { name: "Darth Maul", hP: 180, iAp: 4, aP: 4, cAP: 25, image: "maul" };
-    darthSidious = { name: "Darth Sidious", hP: 150, iAp: 6, aP: 6, cAP: 20, image: "sidious" };
-    lukeSkywalker = { name: "Luke Skywalker", hP: 100, iAp: 32, aP: 32, cAP: 5, image: "luke" };
-    characterArray = [obiWan, darthMaul, darthSidious, lukeSkywalker];
-    yourChar;
-    // enemyArray[];
-    enemyChar;
-    imgSrc = '<img src=assets/img/' + lukeSkywalker.image + '.jpg>';
+    $("#your-Character-Box").empty();
+    $("#defender-Box-Area").empty();
+    $("#fight-Button").unbind("click");
+    startGame();
 }
 function moveChar(c1) {
     var charClass = c1.image;
@@ -144,12 +144,20 @@ function chooseEnemy(c1) {
 }
 
 // fight function 
-function fight() {
-    yourChar.hP = yourChar.hP - enemyChar.cAP;
-    enemyChar.hP = enemyChar.hP - yourChar.aP;
+function fight() {    
+    enemyChar.hP = enemyChar.hP - yourChar.aP;  
+    if (enemyChar.hP <= 0) {
+        victory();
+    }  
+    else{
+        yourChar.hP = yourChar.hP - enemyChar.cAP;
+    }
+    
     console.log("You did " + yourChar.aP + " damage to " + enemyChar.name);
     console.log(enemyChar.name + " did " + enemyChar.cAP + " damage to you.");
     yourChar.aP = yourChar.aP + yourChar.iAp;
+    //update defender zone text
+   // $("#defender-zone")
     console.log(yourChar.hP);
     console.log(enemyChar.hP);
     $("."+yourChar.image).find(".hp-box").text("HP = "+yourChar.hP)
@@ -157,15 +165,13 @@ function fight() {
     if (yourChar.hP <= 0) {
         gameOver();
     }
-    else if (enemyChar.hP <= 0) {
-        victory();
-
-    }
+    
 }
 function gameOver() {
     //display lossText
     //add restart button
     console.log("you lose");
+    phase=0;
     reset();
 }
 function victory() {
